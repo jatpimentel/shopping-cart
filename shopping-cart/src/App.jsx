a1d35e36c45f387e1c6c3b52815a2674046192fb
jatpimentel <jobaaron444@gmail.com>
2024-11-01 21:59:53 +0800

    Added a few snippets para masundog ang store na repo

--- shopping-cart/src/App.jsx
+++ shopping-cart/src/App.jsx
@@ -2,9 +2,35 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const getProducts = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://fakestoreapi.com/products?sort=${sort}`,
+          { signal }
+        );
+        const data = await response.json();
+
+        setProducts(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+    return () => {
+      controller.abort();
+    };
+  }, [sort]);
 
   return <></>;
 }
